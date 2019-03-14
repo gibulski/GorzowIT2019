bf1a8506b671b2190d339da50ab6a8c93742bea7
Tomek G <tomekgib@pro.onet.pl>
2019-03-14 13:31:19 +0100

    Add project files.

---
+++ GorzowIT2019/GorzowIT2019/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GorzowIT2019
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Wyraź swoją opinię", "Czy spotkania GorzówIT są ok?", "Tak", "Nie");
+        }
+
+        private async void OpenPageButton_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new LoginPage());
+
+            await Navigation.PushAsync(new LoginPage());
+
+        }
+
+
+    }
+}
